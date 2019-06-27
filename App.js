d5cebb41d42d04c803a6d51790b13cece1bc87d4
vikrantnegi <vikrantnegi007@gmail.com>
2019-06-27 18:35:57 +0530

    add translation and main code

--- App.js
+++ App.js
@@ -1,49 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-use-before-define */
+/* eslint-disable global-require */
+import React from 'react';
+import * as RNLocalize from 'react-native-localize';
+import i18n from 'i18n-js';
+import memoize from 'lodash.memoize';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { I18nManager, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const translationGetters = {
+  // lazy requires (metro bundler does not support symlinks)
+  ar: () => require('./src/translations/ar.json'),
+  en: () => require('./src/translations/en.json'),
+  fr: () => require('./src/translations/fr.json'),
+};
 
-type Props = {};
-export default class App extends Component<Props> {
+const translate = memoize(
+  (key, config) => i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key)
+);
+
+const setI18nConfig = () => {
+  // fallback if no available language fits
+  const fallback = { languageTag: 'en', isRTL: false };
+
+  const { languageTag, isRTL } = RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) || fallback;
+
+  // clear translation cache
+  translate.cache.clear();
+  // update layout direction
+  I18nManager.forceRTL(isRTL);
+
+  // set i18n-js config
+  i18n.translations = { [languageTag]: translationGetters[languageTag]() };
+  i18n.locale = languageTag;
+};
+
+export default class SyncExample extends React.Component {
+  constructor(props) {
+    super(props);
+    setI18nConfig(); // set initial config
+  }
+
+  componentDidMount() {
+    RNLocalize.addEventListener('change', this.handleLocalizationChange);
+  }
+
+  componentWillUnmount() {
+    RNLocalize.removeEventListener('change', this.handleLocalizationChange);
+  }
+
+  handleLocalizationChange = () => {
+    setI18nConfig();
+    this.forceUpdate();
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Line name="RNLocalize.getLocales()" value={RNLocalize.getLocales()} />
+          <Line name="RNLocalize.getCurrencies()" value={RNLocalize.getCurrencies()} />
+          <Line name="RNLocalize.getCountry()" value={RNLocalize.getCountry()} />
+          <Line name="RNLocalize.getCalendar()" value={RNLocalize.getCalendar()} />
+          <Line name="RNLocalize.getNumberFormatSettings()" value={RNLocalize.getNumberFormatSettings()} />
+          <Line name="RNLocalize.getTemperatureUnit()" value={RNLocalize.getTemperatureUnit()} />
+          <Line name="RNLocalize.getTimeZone()" value={RNLocalize.getTimeZone()} />
+          <Line name="RNLocalize.uses24HourClock()" value={RNLocalize.uses24HourClock()} />
+          <Line name="RNLocalize.usesMetricSystem()" value={RNLocalize.usesMetricSystem()} />
+          <Line
+            name="RNLocalize.findBestAvailableLanguage(['en-US', 'en', 'fr'])"
+            value={RNLocalize.findBestAvailableLanguage(['en-US', 'en', 'fr'])}
+          />
+
+          <Line name="Translation example" value={translate('hello')} />
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
+const Line = props => (
+  <View style={styles.block}>
+    <Text style={styles.name}>{props.name}</Text>
+    <Text style={styles.value}>{JSON.stringify(props.value, null, 2)}</Text>
+  </View>
+);
+
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
+    backgroundColor: 'white',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  container: {
+    padding: 16,
+    alignItems: 'flex-start',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  block: {
+    marginBottom: 16,
+    alignItems: 'flex-start',
+  },
+  name: {
+    textDecorationLine: 'underline',
+    fontWeight: '500',
+    marginBottom: 8,
+  },
+  value: {
+    textAlign: 'left',
   },
 });
